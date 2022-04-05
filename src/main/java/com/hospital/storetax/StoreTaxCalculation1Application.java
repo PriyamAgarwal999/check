4d0a98e84457bbf3a7dfcef69b7972e8184b4801
Priyam <priyamagarwal999@gmail.com>
2022-04-06 03:35:09 +0530

    changes

--- src/main/java/com/hospital/storetax/StoreTaxCalculation1Application.java
+++ src/main/java/com/hospital/storetax/StoreTaxCalculation1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreTaxCalculation1Application {
+public class StoredkndknmTaxCalculation1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreTaxCalculation1Application.class, args);
