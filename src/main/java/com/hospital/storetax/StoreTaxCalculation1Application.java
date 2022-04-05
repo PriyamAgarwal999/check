90e8d3bb8c6bced002d3c25044952b827f7efab5
Priyam <priyamagarwal999@gmail.com>
2022-04-06 04:13:03 +0530

    yes

--- src/main/java/com/hospital/storetax/StoreTaxCalculation1Application.java
+++ src/main/java/com/hospital/storetax/StoreTaxCalculation1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoredkndknmTaxCalculation1Application {
+public class StoreTaxCalculation1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreTaxCalculation1Application.class, args);
