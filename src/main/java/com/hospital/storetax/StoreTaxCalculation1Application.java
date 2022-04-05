9ef112a58413a9ba436e9a68901566af129746f3
Priyam <priyamagarwal999@gmail.com>
2022-04-06 03:31:30 +0530

    h

---
+++ src/main/java/com/hospital/storetax/StoreTaxCalculation1Application.java
@@ -0,0 +1,13 @@
+package com.hospital.storetax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreTaxCalculation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreTaxCalculation1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/storetax/StoreTaxCalculation1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.storetax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreTaxCalculation1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
